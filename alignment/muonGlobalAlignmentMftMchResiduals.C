c8db3a7c5882428f9e2ec38bdef8062da0a93ff1
aferrero2707 <aferrero1975@gmail.com>
2026-06-20 19:15:44 +0200

    Fixed momentum range for DE shift computation
    By mistake the DE shifts from the MFT tracks were computed considering the
    full track momentum range, instead of selecting p > 50 GeV/c

--- alignment/muonGlobalAlignmentMftMchResiduals.C
+++ alignment/muonGlobalAlignmentMftMchResiduals.C
@@ -2055,6 +2055,25 @@ void GetDEShifts(THnSparse* hDEx, THnSparse* hDEy, std::map<int, std::array<std:
   // DE residuals
   //-------------------
 
+#ifdef DE_GROUPING_V0
+  std::vector<std::tuple<std::string, int, std::string, int, std::vector<int>>> deGroups {
+    {"CH1", 1, "CH", 0, {100, 101, 102, 103}},
+    {"CH2", 2, "CH", 0, {200, 201, 202, 203}},
+    {"CH3", 3, "CH", 0, {300, 301, 302, 303}},
+    {"CH4", 4, "CH", 0, {400, 401, 402, 403}},
+    {"CH5", 5, "CH", 0, {505, 506, 507, 508, 509, 510, 511, 512, 513, 500, 501, 502, 503, 504, 514, 515, 516, 517}},
+    {"CH6", 6, "CH", 0, {605, 606, 607, 608, 609, 610, 611, 612, 613, 600, 601, 602, 603, 604, 614, 615, 616, 617}},
+    {"CH7", 7, "CH", 0, {707, 708, 709, 710, 711, 712, 713, 714, 715, 716, 717, 718, 719, 700, 701, 702, 703, 704, 706, 707, 720, 721, 722, 723, 724, 725}},
+    {"CH8", 8, "CH", 0, {807, 808, 809, 810, 811, 812, 813, 814, 815, 816, 817, 818, 819, 800, 801, 802, 803, 804, 806, 807, 820, 821, 822, 823, 824, 825}},
+    {"CH9", 9, "CH", 0, {907, 908, 909, 910, 911, 912, 913, 914, 915, 916, 917, 918, 919, 900, 901, 902, 903, 904, 906, 907, 920, 921, 922, 923, 924, 925}},
+    {"CH10", 10, "CH", 0, {1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1015, 1016, 1017, 1018, 1019, 1000, 1001, 1002, 1003, 1004, 1006, 1007, 1020, 1021, 1022, 1023, 1024, 1025}}
+  };
+
+  std:vector<std::string> deGroupNames{
+    "CH"
+  };
+#endif
+
 #ifdef DE_GROUPING_V1
   std::vector<std::tuple<std::string, int, std::string, int, std::vector<int>>> deGroups {
     {"DE100", 1, "Q0", 0, {100}},
@@ -2238,9 +2257,6 @@ void GetDEShifts(THnSparse* hDEx, THnSparse* hDEy, std::map<int, std::array<std:
   // 4: momentum
   // 5: track slope
 
-  // -----------------
-  // X direction
-
   THnSparse* hDE[2] = { hDEx, hDEy };
   if (hDE[0] && hDE[1]) {
 
@@ -2269,6 +2285,7 @@ void GetDEShifts(THnSparse* hDEx, THnSparse* hDEy, std::map<int, std::array<std:
       hDE[xy]->GetAxis(1)->SetRange(0, -1);
       hDE[xy]->GetAxis(2)->SetRange(0, -1);
       hDE[xy]->GetAxis(3)->SetRange(0, -1);
+
       int momBin = 0;
       int momBinMax = -1;
       if (pAxisId >= 0) {
@@ -2356,6 +2373,24 @@ void GetDEShifts(THnSparse* hDEx, THnSparse* hDEy, std::map<int, std::array<std:
 
       // loop over coordinate
       for (int xy = 0; xy < 2; xy++) {
+
+        for (int dim = 0; dim < hDE[xy]->GetNdimensions(); dim++) {
+          hDE[xy]->GetAxis(dim)->SetRange(0, hDE[xy]->GetAxis(dim)->GetNbins());
+        }
+
+        // set momentum range
+        int momBin = 0;
+        int momBinMax = -1;
+        if (pAxisId >= 0) {
+          // p >= xx GeV/c
+          momBin = hDE[xy]->GetAxis(pAxisId)->FindBin(mchMomMin + epsilon);
+          momBinMax = hDE[xy]->GetAxis(pAxisId)->GetNbins();
+          hDE[xy]->GetAxis(pAxisId)->SetRange(momBin, momBinMax);
+        }
+
+        //std::cout << std::format("Setting range of quadrantAxisId={} to (0, 1)", quadrantAxisId) << std::endl;
+        //hDE[xy]->GetAxis(quadrantAxisId)->SetRangeUser(0, 1);
+
         hGroupResiduals[xy].emplace_back(std::array<TH1*, 2>{nullptr, nullptr});
         if (slopeAxisId >= 0) {
           hGroupResidualsVsSlope[xy].emplace_back(std::array<TH2*, 2>{nullptr, nullptr});
@@ -2551,6 +2586,14 @@ void GetDEShifts(THnSparse* hDEx, THnSparse* hDEy, std::map<int, std::array<std:
       mg->SetMaximum(1);
       legend->Draw();
       c5.SaveAs(residualsGroupsFileName.c_str());
+      /*mg->SetMinimum(-0.5);
+      mg->SetMaximum(0.5);
+      legend->Draw();
+      c5.SaveAs(residualsGroupsFileName.c_str());
+      mg->SetMinimum(-0.1);
+      mg->SetMaximum(0.1);
+      legend->Draw();
+      c5.SaveAs(residualsGroupsFileName.c_str());*/
     }
 
     // loop over coordinate
@@ -2594,6 +2637,14 @@ void GetDEShifts(THnSparse* hDEx, THnSparse* hDEy, std::map<int, std::array<std:
       mg->Draw("a");
       mg->SetMinimum(-1);
       mg->SetMaximum(1);
+      legend->Draw();
+      c5.SaveAs(residualsGroupsFileName.c_str());
+      mg->SetMinimum(-0.5);
+      mg->SetMaximum(0.5);
+      legend->Draw();
+      c5.SaveAs(residualsGroupsFileName.c_str());
+      mg->SetMinimum(-0.1);
+      mg->SetMaximum(0.1);
       legend->Draw();
       c5.SaveAs(residualsGroupsFileName.c_str());
     }
@@ -2641,6 +2692,10 @@ void GetDEShifts(THnSparse* hDEx, THnSparse* hDEy, std::map<int, std::array<std:
       //mg->SetMaximum(1);
       mg->SetMinimum(-0.5);
       mg->SetMaximum(0.5);
+      legend->Draw();
+      c5.SaveAs(residualsGroupsFileName.c_str());
+      mg->SetMinimum(-0.1);
+      mg->SetMaximum(0.1);
       legend->Draw();
       c5.SaveAs(residualsGroupsFileName.c_str());
     }
